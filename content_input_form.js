4d901930602f2da6fe9517706c6bf23844708501
IMP1 <IMP1@mail.com>
2019-07-12 15:30:32 +0100

    add styling of input forms

--- content_input_form.js
+++ content_input_form.js
@@ -1,10 +1,21 @@
 function setup_input_form_content(note_container, note) {
     let form = document.createElement("section");
     form.classList.add("note-body");
+    form.classList.add("input-form-container");
+    form.classList.add("row");
 
     for (key in note.content) {
         let input_field = document.createElement("div");
-        input_field.classList.add("input-field", "col");
+        input_field.classList.add("input-field", "col", "s6");
+        if (note.content_layout_data[key].position !== undefined) {
+            input_field.style.position = "absolute";
+            input_field.style.left = note.content_layout_data[key].position[0];
+            input_field.style.top = note.content_layout_data[key].position[1];
+        }
+        if (note.content_layout_data[key].size !== undefined) {
+            input_field.style.width = note.content_layout_data[key].size[0];
+            input_field.style.height = note.content_layout_data[key].size[1];
+        }
 
         let input = document.createElement("input");
         input.setAttribute("id", "note-content-" + key);
@@ -14,7 +25,7 @@ function setup_input_form_content(note_container, note) {
 
         let label = document.createElement("label");
         label.setAttribute("for", input.id);
-        label.textContent = key;
+        label.textContent = note.content_layout_data[key].label;
         input_field.appendChild(label);
 
         form.appendChild(input_field);
