7bc49b8416e044fe9558fcf141f978a545349cf5
IMP1 <IMP1@mail.com>
2019-07-12 11:28:45 +0100

    factor out markdown into own content file

---
+++ content_markdown.js
@@ -0,0 +1,8 @@
+function setup_markdown_content(note_container, note) {
+    let body = document.createElement("section");
+    body.setAttribute("contenteditable", true);
+    body.classList.add("note-body");
+    body.classList.add("markdown");
+    body.textContent = note.content;
+    note_container.appendChild(body);
+}
